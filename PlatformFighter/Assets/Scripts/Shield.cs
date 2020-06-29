87b1654b914bfd0e0a85cc06defbc7a979903f70
Kai Yi (David) Zhang <theyijimima@gmail.com>
2020-06-29 18:00:23 -0400

    added shield

---
+++ PlatformFighter/Assets/Scripts/Shield.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    Vector3 scale;
+    const float SHIELDDURATION = 2f;
+    const float RECHARGETIME = 4f;
+    float currentTime = 2f;
+    float ratio = 1f;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        scale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+        ratio = currentTime / SHIELDDURATION;
+        transform.localScale = new Vector3(scale.x * ratio, scale.y * ratio, scale.z * ratio);
+    }
+}
