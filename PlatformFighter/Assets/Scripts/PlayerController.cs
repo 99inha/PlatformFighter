59a96011ade70d42b224c4bde7f3cd3ac8248862
99inha <38464925+99inha@users.noreply.github.com>
2020-06-29 17:35:56 -0400

    Implementing Basic Movement

--- PlatformFighter/Assets/Scripts/PlayerController.cs
+++ PlatformFighter/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UsefulConstants;
 
@@ -13,16 +15,111 @@ public class PlayerController : MonoBehaviour
     // public fields
     public PlayerHealth health;
     public AnimeState currState; // enum class AnimeState from UsefulConstants
+    public float moveSpeed = 5f;
+    public float jumpSpeed = 12f;
+    public float fallMaxSpeed = -12f;
+    public Vector2 velocity;
+
+    // private fields
+    Rigidbody2D rb;
+    Vector3 localScale;
+    bool isGrounded = false;
+    int jumpCount = 1;
+    bool hasControl = true;
+
+
+    // Unity basic functions
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        localScale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        velocity = rb.velocity;
+        correctJumpCount();
+
+        if (hasControl)
+        {
+            computeHorizontalMovement();
+            computeVerticalMovement();
+        }
+    }
+
+    // OnCollisionEnter2D is called whenever another collider hits this object
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.transform.tag == "Ground")
+        {
+            isGrounded = true;
+            jumpCount = 2;
+        }
+    }
+
+
+
+
+    // Movement computation
+
+    void computeHorizontalMovement()
+    {
+        float inputDirection = Input.GetAxisRaw("Horizontal");
+
+        if (inputDirection != 0)
+        {
+            localScale.x = inputDirection;
+            transform.localScale = localScale;
+        }
+        rb.velocity = new Vector2(moveSpeed * inputDirection, rb.velocity.y);
+    }
+
+    void computeVerticalMovement()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            isGrounded = false;
+            jumpCount--;
+        }
+
+        if (rb.velocity.y < fallMaxSpeed)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, fallMaxSpeed);
+        }
+    }
+
+
+
+
+    /* correctJumpCount:
+     *    corrects the jump count and groundedness in case a player walks off stage
+     *    without jumping
+     */
+    void correctJumpCount()
+    {
+        if (isGrounded && Mathf.Abs(rb.velocity.y) > 0.05f)
+        {
+            isGrounded = false;
+            jumpCount = 1;
+        }
+    }
+
+    /* lagForSeconds
+     *     strips the control over the character for a given amount of seconds
+     *     Args: float lagTime: how long the character lags
+     *     Returns: IEnumerator for StartCoroutine
+     *     Usage: StartCoroutine("lagForSeconds", lagTime);
+     */
+    public IEnumerator lagForSeconds(float lagTime)
+    {
+        hasControl = false;
+            
+        yield return new WaitForSeconds(lagTime);
+
+        hasControl = true;
     }
 }
