5dc6632406211ad31e3fb7efcc59016809b638fe
99inha <38464925+99inha@users.noreply.github.com>
2020-06-29 11:22:30 -0400

    Initial design implementation

---
+++ PlatformFighter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿/* This class functions as the controller for the player
+ * Any common mechanics will be implemented here
+ * ex) movement, jump, death, respawn
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UsefulConstants;
+
+public class PlayerController : MonoBehaviour
+{
+    // public fields
+    public PlayerHealth health;
+    public AnimeState currState; // enum class AnimeState from UsefulConstants
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PlatformFighter/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,39 @@
+﻿/* This class acts as the model for the player
+ * This class will store common information that the player needs
+ * The controller class will manipulate the values here
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    // public fields; mutable through unity editor
+    public int maxHealth;
+
+    // private fields
+    private int currHealth;
+
+    /* getHealth:
+     *   getter for the current health of the player
+     *      Args: none
+     *      Returns: current health of the player
+     */
+    public int getHealth()
+    {
+        return currHealth;
+    }
+
+    /* takeDamage:
+     *   decrements the health of the player based on the move used
+     *      Args: a kind of data structure to carry the kind of move or
+     *            the amount of damage taken, to decrement the health by
+     *            (unimplemented yet! need to implement)
+     *      Returns: nothing
+     */
+    public void takeDamage()
+    {
+
+    }
+}
