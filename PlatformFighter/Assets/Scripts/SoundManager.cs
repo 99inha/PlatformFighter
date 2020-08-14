bd08868bea8d07d6b78c7a4340afa435b1c0f9aa
Kai Yi (David) Zhang <61890246+KaiYi-David-Zhang@users.noreply.github.com>
2020-08-14 13:01:39 -0400

    added sound effect to start screen

---
+++ PlatformFighter/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public AudioClip startGame;
+    AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    //
+    public void playSound(string name)
+    {
+        if(string.Compare(name, "start") == 0)
+        {
+            audio.PlayOneShot(startGame);
+        }
+    }
+}
